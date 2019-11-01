d13c7e3e50460df02e438ed209607559e0b3eda3
Jarship <JohnArthurBishop@gmail.com>
2019-11-01 12:10:52 -0400

    Update text for labels

--- theme.js
+++ theme.js
@@ -3,7 +3,13 @@ export const colors = {
   khaki: "#C9B79C",
   lightFrenchBeige: "#D0B17A",
   oldSilver: "#848586",
-  smoke: "#71816D"
+  smoke: "#71816D",
+  white: "#FAFAFA",
+  black: [
+    "#000",
+    "#121212",
+    "#242424"
+  ]
 };
 
 export const breakpoints = ["320px", "768px", "1024px", "1274px"];
@@ -21,7 +27,38 @@ export const space = [
   "96px",
   "128px",
   "256px"
-]
+];
+
+export const texts = {
+  h2: {
+    color: colors.black[0],
+    fontSize: "28px",
+    fontWeight: "600",
+    fontStyle: "normal",
+    lineHeight: "42px"
+  },
+  label: {
+    color: colors.black[1],
+    fontSize: "14px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    lineHeight: "18px",
+    textAlign: "center"
+  },
+  common: {
+    color: colors.black[2],
+    fontSize: "12px",
+    fontWeight: "normal",
+    fontStyle: "normal",
+    lineHeight: "16px"
+  }
+};
+
+export const fields = {
+  primary : {
+    backgroundColor: colors.white
+  }
+};
 
 export const globalStyles = `
   html,
@@ -44,5 +81,8 @@ export const globalStyles = `
 `;
 
 export default {
-  colors
+  colors,
+  breakpoints,
+  space,
+  texts
 };
