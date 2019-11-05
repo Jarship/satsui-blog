2b5ab461012efccee268a785375b0baf856b900a
Jarship <JohnArthurBishop@gmail.com>
2019-11-04 20:18:20 -0500

    Make some basic updates

--- pages/signup.js
+++ pages/signup.js
@@ -1,23 +1,17 @@
 import { Flex } from "rebass";
-import Link from "next/link";
+import Link from "../components/lib/link";
 import Layout from "../components/layout";
 import Register from "../components/register";
-import redirect from "../lib/redirect";
 
-const Signup = ({ inviteCode }) => {
+const Signup = () => {
   return (
     <Layout>
       <Flex width={1} justifyContent="center" alignItems="center" flexDirection="column">
-        <Register inviteCode={inviteCode} />
+        <Register bg="oldRose" width={[1, 1, 1 / 2, 1 / 3]} />
         <Link href="/login"><a>Looking for Log In?</a></Link>
       </Flex>
     </Layout>
   );
 };
-
-Signup.getInitialProps = async context => {
-  const { inviteCode } = context.query;
-  return { inviteCode };
-}
 
 export default Signup;
