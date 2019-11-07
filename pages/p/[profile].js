d22515b2f20eccba41d21940fef051b742c1df63
Jarship <JohnArthurBishop@gmail.com>
2019-11-06 20:11:14 -0500

    Adjust information for profiles

--- pages/p/[profile].js
+++ pages/p/[profile].js
@@ -1,33 +1,21 @@
-import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useState, useEffect } from 'react';
-import fetch from 'isomorphic-unfetch';
+import { Flex } from 'rebass';
 import { handleLoggedIn } from '../../lib/getUser';
 import Layout from '../../components/layout';
 import Image from '../../components/lib/image';
 import Text from '../../components/lib/text';
 import { UPLOAD_PROFILE_PICTURE } from '../../lib/mutations';
 import FileUpload from '../../components/profileImageUpload';
+import { PROFILE_QUERY } from '../../lib/queries';
 
-const Profile = () => {
-  const [user, setUser] = useState(null); // Current User
+const Profile = ({ userProfileUrl, current }) => {
   const [uploadFailure, setUploadFailure] =  useState(null); // Errors from uploading
   const [uploadProgress, setUploadProgress] = useState(0); // upload progress for picture
-  const client = useApolloClient(); // Apollo State
-
-  const fetchUser = async () => {
-    const results = await handleLoggedIn(client);
-    setUser(results.user);
-  };
-
-  //This useEffect is used to get the current user for display
-  useEffect(() => {
-    fetchUser();
-  }, []);
 
   // onCompleted is for the mutation that fetches the Upload URL
   const onCompleted = async data => {
     if (!data.uploadProfilePicture.error) {
-      fetchUser();
     } else {
       setUploadFailure(data.uploadProfilePicture.error.message);
     }
@@ -56,20 +44,44 @@ const Profile = () => {
     setUploadProgress(50);
     uploadProfilePicture({ variables: { file: local } });
     setUploadProgress(100);
-  }
+  };
+
+  const { loading, error, data } = useQuery(PROFILE_QUERY, {
+    variables: { url: userProfileUrl },
+    ssr: true
+  });
+
+  if (error) return <Layout><p>Error loading profile.</p></Layout>;
+  if (loading) return <div>Loading...</div>
+
+  const { profile: user } = data;
+  const ProfileImage = <Image variant="profile" source={user.photo} altText="Profile Picture" />;
 
   return (
     <Layout>
-      {user &&
-        <FileUpload
-          loading={uploadProgress}
-          image={(<Image variant="profile" source={user.photo} altText="Profile Picture" />)}
-          onFileChange={handlePictureUpload}
-        />
-      }
-      {uploadFailure && <Text type="error">{uploadFailure}</Text>}
+      <Flex flexDirection="column" alignItems="center">
+        <Text type="h2">{user.name}</Text>
+        <Flex alignSelf="flex-start" ml={4}>
+          {userProfileUrl === current.url ?
+            <FileUpload
+              loading={uploadProgress}
+              image={ProfileImage}
+              onFileChange={handlePictureUpload}
+            />
+            :
+            ProfileImage
+          }
+        </Flex>
+        {uploadFailure && <Text type="error">{uploadFailure}</Text>}
+      </Flex>
     </Layout>
   );
+};
+
+Profile.getInitialProps = async context => {
+  const currentUser = await handleLoggedIn(context.apolloClient);
+  const url = context.query.profile;
+  return { userProfileUrl: url, current: currentUser.user };
 };
 
 export default Profile;
